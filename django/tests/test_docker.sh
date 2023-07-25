69a83b886ebe708f6253c4a4b7d3446caa27cdd2
DKSHS <88355080+ShadowsS01@users.noreply.github.com>
2023-07-25 13:50:02 -0300

    feat: adding project tests in test_docker.sh

--- django/tests/test_docker.sh
+++ django/tests/test_docker.sh
@@ -17,6 +17,12 @@ cd my_awesome_project
 # make sure all images build
 docker compose -f local.yml build
 
+# run the project's type checks
+docker compose -f local.yml run django mypy my_awesome_project
+
+# run the project's tests
+docker compose -f local.yml run django pytest
+
 # return non-zero status code if there are migrations that have not been created
 docker compose -f local.yml run django python manage.py makemigrations --dry-run --check || {
   echo "ERROR: there were changes in the models, but migration listed above have not been created and are not saved in version control"
